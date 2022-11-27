754665a25eb1f382762a92398e5ad42a26f21c05
Ajoy Mahato <ajoymahato16@gmail.com>
2022-11-27 19:58:21 +0530

    Onlinetest project intial commmit

---
+++ onlinetest/src/app/usertest/usertest.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsertestComponent } from './usertest.component';
+
+describe('UsertestComponent', () => {
+  let component: UsertestComponent;
+  let fixture: ComponentFixture<UsertestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UsertestComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UsertestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ onlinetest/src/app/usertest/usertest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usertest',
+  templateUrl: './usertest.component.html',
+  styleUrls: ['./usertest.component.scss']
+})
+export class UsertestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
